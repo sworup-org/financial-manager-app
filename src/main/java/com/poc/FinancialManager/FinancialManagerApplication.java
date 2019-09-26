9fc86f06ade9b999b8092e05305e963e12b0f897
Patra <sworup.patra@sap.com>
2019-09-27 00:42:26 +0530

    Adding Inital framework for poc development

---
+++ src/main/java/com/poc/FinancialManager/FinancialManagerApplication.java
@@ -0,0 +1,13 @@
+package com.poc.FinancialManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialManagerApplication.class, args);
+	}
+
+}
