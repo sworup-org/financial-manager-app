7819e4567bd0a389f369ff75c6836920730e38d4
sworup1993 <48334494+sworup1993@users.noreply.github.com>
2019-09-30 01:06:33 +0530

    Adding userId key to FinancialModel

--- src/main/java/com/poc/FinancialManager/model/FinancialModel.java
+++ src/main/java/com/poc/FinancialManager/model/FinancialModel.java
@@ -1,7 +1,8 @@
 package com.poc.FinancialManager.model;
 
 public class FinancialModel {
-
+    
+    private int userId
     private IncomeModel incomeModel;
     private ExpenditureModel expenditureModel;
     private int savings;
